142ea1fbcabf1f03919cb43efa64e8b47acba275
Hudson Knott <hudson@hudsonknott.co.uk>
2023-01-18 14:09:12 +0000

    Initial commit for new branch

--- src/main/java/uk/co/hudsonknott/shadowtag/ShadowTag.java
+++ src/main/java/uk/co/hudsonknott/shadowtag/ShadowTag.java
@@ -1,10 +1,7 @@
 package uk.co.hudsonknott.shadowtag;
 
-import uk.co.hudsonknott.shadowtag.commands.STCommands;
-import uk.co.hudsonknott.shadowtag.events.OnDamage;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.IOException;
 import java.util.Properties;
 
@@ -18,13 +15,8 @@ public final class ShadowTag extends JavaPlugin {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //getCommand("shadowtagger").setExecutor(new SetTagger());
-        //getCommand("shadowend").setExecutor(new EndGame());
-        getCommand("shadowtag").setExecutor(new STCommands());
-        getServer().getPluginManager().registerEvents(new OnDamage(), this);
+
         String version = properties.getProperty("version");
-        String artifactId = properties.getProperty("artifactId");
-        String groupId = properties.getProperty("groupId");
         System.out.println(ChatColor.GOLD + "ShadowTag " + ChatColor.GREEN +"v" + version + ChatColor.GOLD + " is loaded. This is an ALPHA build; please report any bugs in the GitHub issues tab" + ChatColor.GREEN + " (https://github.com/UppishBuzzard87/ShadowTag/issues). " + ChatColor.GOLD + "Happy playing!");
     }
 
